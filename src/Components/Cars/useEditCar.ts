0bd03d2585cc53f4f4f3ae0b8f7d807db0fcac74
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-10-18 01:49:09 +0200

    update cars immediatlety after edit

--- src/Components/Cars/useEditCar.ts
+++ src/Components/Cars/useEditCar.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { editCarAPI } from "../../Services/apiCars";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
@@ -10,11 +10,14 @@ interface EditCarParams {
 }
 
 function useEditCar() {
-  const dispatch = useDispatch();
+    const queryClient = useQueryClient();
   const { mutate: editCar, isPending:isLoading } = useMutation({
     mutationFn: ({ carInputs, carId }: EditCarParams) => editCarAPI(carInputs, carId),
     onSuccess: () => {
       toast.success("Car updated successfully.");
+      queryClient.invalidateQueries({
+        queryKey: ["cars"],
+      });
     },
     onError: (error: any) => {
       toast.error(`Update failed: ${error.message}`);
